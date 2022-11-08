d0e47ea2654141f1532e3b728c9af9906767bc6a
Jongheon Kim <simba3447@gmail.com>
2022-11-09 00:02:19 +0900

    Optimize imports

--- src/main.py
+++ src/main.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.parameters import Port
+
 from vehicle import Vehicle
 
 vehicle = Vehicle(
