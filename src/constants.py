e88718f28ab1b3e90bccfa5e74fd00cd916b9704
Jongheon Kim <simba3447@gmail.com>
2022-12-16 17:28:44 +0900

    Extract method reading and checking message from mail box

--- src/constants.py
+++ src/constants.py
@@ -1,12 +1,2 @@
-# Bluetooth Mailbox Message Code
-
 MAILBOX_NAME = 'greeting'
 SERVER_VEHICLE_NAME = 'ksy'
-
-START_SIGN = 'start'
-STOP_SIGN = 'blue'
-OBSTACLE_DETECTED_SIGN = 'ob'
-SCHOOL_ZONE_SIGN = 'yellow'
-LAB_END_SIGN = 'red'
-PARKING_LOT_DETECTED = 'parking-lot-detected'
-PARKING_ENDED = 'parking-ended'
